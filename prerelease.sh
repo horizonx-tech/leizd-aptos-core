29ebea99cad69463d2ce8edd3cc79b33d05dbce9
hide-yoshi <hide-yoshi@users.noreply.github.com>
2022-09-19 18:06:47 +0900

    deploy to testnet

--- prerelease.sh
+++ prerelease.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sed -i "s/\"TEST_PRIVATE_KEY\"/\"$TEST_PRIVATE_KEY\"/g" .aptos/config.test.yaml
 cp .aptos/config.test.yaml .aptos/config.yaml
-export OWNER=e9882582cd52c6ed40a8d205502c2231899f8495a1655d05c0fa135329508b65
+export OWNER=abfa2e2a861e08a8d6fc34465565e00595913ac1605ef74f9191b7bbc537f759
 aptos account fund-with-faucet --account ${OWNER}
-for pkg in `ls | grep leizd-aptos`; do sed -i "s/\"0x0123456789ABCDEF\"/\"$OWNER\"/g" ${pkg}/Move.toml; done
+for pkg in `ls | grep leizd-aptos`; do sed -i '' "s/\"0x0123456789ABCDEF\"/\"$OWNER\"/g" ${pkg}/Move.toml; done
 for pkg in `ls | grep leizd-aptos`; do aptos move compile --package-dir ${pkg} ; done
