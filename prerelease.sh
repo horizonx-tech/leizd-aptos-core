db34c16579bc1a21c3f65b5b01f3ba34961f4fe9
hide-yoshi <hide-yoshi@users.noreply.github.com>
2022-09-19 18:07:58 +0900

    update script

--- prerelease.sh
+++ prerelease.sh
@@ -3,5 +3,5 @@ sed -i "s/\"TEST_PRIVATE_KEY\"/\"$TEST_PRIVATE_KEY\"/g" .aptos/config.test.yaml
 cp .aptos/config.test.yaml .aptos/config.yaml
 export OWNER=abfa2e2a861e08a8d6fc34465565e00595913ac1605ef74f9191b7bbc537f759
 aptos account fund-with-faucet --account ${OWNER}
-for pkg in `ls | grep leizd-aptos`; do sed -i '' "s/\"0x0123456789ABCDEF\"/\"$OWNER\"/g" ${pkg}/Move.toml; done
+for pkg in `ls | grep leizd-aptos`; do sed -i "s/\"0x0123456789ABCDEF\"/\"$OWNER\"/g" ${pkg}/Move.toml; done
 for pkg in `ls | grep leizd-aptos`; do aptos move compile --package-dir ${pkg} ; done
