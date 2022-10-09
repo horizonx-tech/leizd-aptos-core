9b5b2aecf101be7f0f8dabe0ecb70e1904dbcaff
hide-yoshi <hide-yoshi@users.noreply.github.com>
2022-10-09 13:54:29 +0900

    update cd

--- push_new_key_to_test.sh
+++ push_new_key_to_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+mkdir leizd-aptos-test/.aptos/
 cp .aptos/config.yaml leizd-aptos-test/.aptos/config.devnet.yaml
 cd leizd-aptos-test
-git add .aptos/config.devnet.yaml
+git add -f .aptos/config.devnet.yaml 
+git config --global user.email "hide-yoshi@horizonx.tech"
+git config --global user.name "hide-yoshi"
 git commit -m 'update account'
 git push
