c28c28b4c0b53a309356e4b3b95a6e672cb00df9
hide-yoshi <hide-yoshi@users.noreply.github.com>
2022-10-09 13:29:15 +0900

    update cdf

--- push_new_key_to_test.sh
+++ push_new_key_to_test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-git clone https://github.com/horizonx-tech/leizd-aptos-test.git
 cp .aptos/config.yaml leizd-aptos-test/.aptos/config.devnet.yaml
 cd leizd-aptos-test
 git add .aptos/config.devnet.yaml
