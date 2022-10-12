376e53ee2c0049fd805cf2cd30d3bd6fe3b2dffb
hide-yoshi <hide-yoshi@users.noreply.github.com>
2022-10-12 17:14:53 +0900

    test

--- prerelease.sh
+++ prerelease.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export OWNER=$(aptos account lookup-address |jq -r '.Result')
+export OWNER=$(cat .aptos/config.yaml|grep -v -|yq -P .profiles.default.account)
 aptos account fund-with-faucet --account ${OWNER} --amount 100000000000
 for pkg in `ls | grep leizd-aptos`; do sed -i "s/\"0x0123456789ABCDEF\"/\"$OWNER\"/g" ${pkg}/Move.toml; done
 for pkg in `ls | grep leizd-aptos`; do aptos move compile --package-dir ${pkg} ; done
