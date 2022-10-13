eb21018f2d4229b39a684ab9cf91bd6cafc8304e
darroze <darroze.ar@gmail.com>
2022-10-13 12:14:55 +0900

    log commit hash on account update

--- push_new_key_to_test.sh
+++ push_new_key_to_test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+COMMIT_HASH=$1
+
 mkdir leizd-aptos-test/.aptos/
 cp .aptos/config.yaml leizd-aptos-test/.aptos/config.devnet.yaml
 cd leizd-aptos-test
 git add -f .aptos/config.devnet.yaml 
 git config --global user.email "hide-yoshi@horizonx.tech"
 git config --global user.name "hide-yoshi"
-git commit -m 'update account'
+git commit -m "update account for ${COMMIT_HASH}"
 git push
