8eef4f5d5266d5c6c7332a6db0b8e3e2f2f053ad
hide-yoshi <hide-yoshi@users.noreply.github.com>
2022-10-14 12:48:23 +0900

    fix script

--- push_new_account_to_indexer.sh
+++ push_new_account_to_indexer.sh
@@ -3,8 +3,8 @@ export OWNER=$(cat .aptos/config.yaml|grep -v -|yq -P .profiles.default.account)
 cd leizd-indexer
 yarn
 export TARGET=$(cat aptos-codegen.json |jq -r |grep money_market|cut -c 6-71)
+sed -i "s/\"${TARGET}\"/\"$OWNER\"/g" aptos-codegen.json
 yarn gen -c aptos-codegen.json
-sed -i "s/\"${TARGET}\"/\"$OWNER\"/g" ${pkg}/Move.toml
 git add .
 git config --global user.email "hide-yoshi@horizonx.tech"
 git config --global user.name "hide-yoshi"
