9ffc69b307cb7f1cf14b55ff911d00262983242e
darroze <darroze.ar@gmail.com>
2022-11-22 17:50:14 +0900

    release to devnet

--- new_account.sh
+++ new_account.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 expect -c "
-spawn aptos init --rest-url https://aptos-testnet.ml/v1 --faucet-url https://faucet.aptos-testnet.ml/ --assume-yes
+spawn aptos init --rest-url https://fullnode.devnet.aptoslabs.com/v1 --faucet-url https://faucet.devnet.aptoslabs.com/ --assume-yes
 expect \"Choose network from\"  
 send \"custom\n\" 
 expect \"Enter your private key as a hex literal\"  
